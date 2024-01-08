139ae483b0b7cd7bab608934f9fbecabc01c230f
sstej88 <sunkara.sai@northeastern.edu>
2024-01-08 03:10:47 -0500

    subnets created

--- network_build.tf
+++ network_build.tf
@@ -16,20 +16,28 @@ resource "aws_internet_gateway" "checkday_internet_gateway" {
 }
 
 resource "aws_subnet" "checkday_private_subnet" {
-  vpc_id     = aws_vpc.checkday_vpc.id
-  cidr_block = "10.0.1.0/24"
+  for_each = {for az in var.availability_zones: az => index(var.availability_zones, az)}
+
+  vpc_id            = aws_vpc.checkday_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.checkday_vpc.cidr_block, 8, each.value)
+  availability_zone = each.key
   map_public_ip_on_launch = false
+
   tags = {
-    Name = "checkday_private_subnet"
+    Name = "checkday_private_subnet_${each.key}"
   }
 }
 
 resource "aws_subnet" "checkday_public_subnet" {
-  vpc_id     = aws_vpc.checkday_vpc.id
-  cidr_block = "10.0.3.0/24"
+  for_each = {for az in var.availability_zones: az => index(var.availability_zones, az) + length(var.availability_zones)}
+
+  vpc_id            = aws_vpc.checkday_vpc.id
+  cidr_block        = cidrsubnet(aws_vpc.checkday_vpc.cidr_block, 8, each.value)
+  availability_zone = each.key
   map_public_ip_on_launch = true
+
   tags = {
-    Name = "checkday_public_subnet"
+    Name = "checkday_public_subnet_${each.key}"
   }
 }
 
@@ -37,4 +45,10 @@ variable "cidr" {
   type        = string
   default     = "10.1.0.0/16"
   description = "CIDR Block for VPC"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of availability zones"
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
