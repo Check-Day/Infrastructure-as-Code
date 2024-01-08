e08e7ecd0e3e0388995c33ae5aca3f77c9793d54
sstej88 <sunkara.sai@northeastern.edu>
2024-01-08 11:32:41 -0500

    parameter manager separated

--- network_build.tf
+++ network_build.tf
@@ -76,7 +76,7 @@ resource "aws_lb" "checkday_load_balancer" {
   security_groups    = [aws_security_group.checkday_load_balancer_security_group.id]
   subnets            = [for subnet in aws_subnet.checkday_public_subnet : subnet.id]
 
-  enable_deletion_protection = true
+  enable_deletion_protection = false
 
   tags = {
     Environment = var.environment
@@ -125,49 +125,6 @@ resource "aws_rds_cluster" "checkday_database" {
   final_snapshot_identifier = "checkday-db-final-snapshot-${formatdate("YYYYMMDDHHmmss", timestamp())}"
 }
 
-locals {
-  host = aws_rds_cluster.checkday_database.endpoint
-  database_username = var.database_username
-  database_password = var.database_password
-  database_name = var.database_name
-  database_dialect = var.database_dialect
-}
-
-resource "aws_ssm_parameter" "DATABASE_HOST" {
-  name  = "DATABASE_HOST"
-  type  = "SecureString"
-  value = local.host
-  overwrite = true
-}
-
-resource "aws_ssm_parameter" "DATABASE_USERNAME" {
-  name  = "DATABASE_USERNAME"
-  type  = "SecureString"
-  value = local.database_username
-  overwrite = true
-}
-
-resource "aws_ssm_parameter" "DATABASE_PASSWORD" {
-  name  = "DATABASE_PASSWORD"
-  type  = "SecureString"
-  value = local.database_password
-  overwrite = true
-}
-
-resource "aws_ssm_parameter" "DATABASE_NAME" {
-  name  = "DATABASE_NAME"
-  type  = "SecureString"
-  value = local.database_name
-  overwrite = true
-}
-
-resource "aws_ssm_parameter" "DATABASE_DIALECT" {
-  name  = "DATABASE_DIALECT"
-  type  = "SecureString"
-  value = local.database_dialect
-  overwrite = true
-}
-
 variable "cidr" {
   type        = string
   default     = "10.1.0.0/16"
@@ -184,28 +141,4 @@ variable "environment" {
   type        = string
   description = "Describes environment"
   default     = "dev"
-}
-
-variable "database_username" {
-  type        = string
-  description = "Database Username"
-  default     = "root"
-}
-
-variable "database_password" {
-  type        = string
-  description = "Database Password"
-  default     = "rootroot"
-}
-
-variable "database_name" {
-  type        = string
-  description = "Database Name"
-  default     = "checkdayclient"
-}
-
-variable "database_dialect" {
-  type        = string
-  description = "Database Dialect"
-  default     = "mysql"
 }
