edf57d46c84a4602082c4841edf4cc3c6eb49413
sstej88 <sunkara.sai@northeastern.edu>
2024-01-11 17:26:20 -0500

    Created Kubernetes Cluster, nodes, open id connector, and initialized tls certificate for eks

---
+++ kubernetes_nodes.tf
@@ -0,0 +1,91 @@
+resource "aws_iam_role" "checkday_node_group_role" {
+  name = "checkday_node_group_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "checkday_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.checkday_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "checkday_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.checkday_node_group_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "checkday_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.checkday_node_group_role.name
+}
+
+resource "aws_security_group" "checkday_worker_node_security_group" {
+    name = "checkday_worker_node_security_group"
+    description = "Allows SSH Inbound Traffic"
+    vpc_id = aws_vpc.checkday_vpc.id
+
+    ingress {
+        description = "SSH access to Subnet"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_eks_node_group" "checkday_eks_private_nodes" {
+    cluster_name = aws_eks_cluster.checkday_eks_cluster.name
+    node_group_name = "checkday_eks_private_nodes"
+    node_role_arn = aws_iam_role.checkday_node_group_role.arn
+
+    subnet_ids = [for subnet in aws_subnet.checkday_private_subnet : subnet.id]
+
+    capacity_type = "ON_DEMAND"
+    instance_types = ["t3.small"]
+
+    scaling_config {
+        desired_size = 1
+        max_size = 7
+        min_size = 0
+    }
+
+    update_config {
+        max_unavailable = 1
+    }
+
+    labels = {
+        role = "general"
+        node = "checkday_k8_node"
+    }
+
+    remote_access {
+    ec2_ssh_key = aws_key_pair.checkday_generated_key_pair.key_name
+    source_security_group_ids = [aws_security_group.checkday_worker_node_security_group.id]
+  }
+
+    depends_on = [
+        aws_iam_role_policy_attachment.checkday_AmazonEKSWorkerNodePolicy,
+        aws_iam_role_policy_attachment.checkday_AmazonEKS_CNI_Policy,
+        aws_iam_role_policy_attachment.checkday_AmazonEC2ContainerRegistryReadOnly,
+    ]
+}
---
+++ kubernetes_openid_connector.tf
@@ -0,0 +1,53 @@
+data "tls_certificate" "checkday_tls_certificate_for_eks" {
+    url = aws_eks_cluster.checkday_eks_cluster.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "checkday_openid_connector" {
+    client_id_list = ["sts.amazonaws.com"]
+    thumbprint_list = [data.tls_certificate.checkday_tls_certificate_for_eks.certificates[0].sha1_fingerprint]
+    url = aws_eks_cluster.checkday_eks_cluster.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "test_oidc_assume_role_policy" {
+    statement {
+        actions = ["sts:AssumeRoleWithWebIdentity"]
+        effect = "Allow"
+
+        condition {
+            test = "StringEquals"
+            variable = "${aws_iam_openid_connect_provider.checkday_openid_connector.url}:aud"
+            values = ["system:serviceaccount:default:aws-test"]
+        }
+
+        principals {
+            identifiers = [aws_iam_openid_connect_provider.checkday_openid_connector.arn]
+            type = "Federated"
+        }
+    }
+}
+
+resource "aws_iam_role" "checkday_test_oidc" {
+    assume_role_policy = data.aws_iam_policy_document.test_oidc_assume_role_policy.json
+    name = "checkday_test_oidc"
+}
+
+resource "aws_iam_policy" "checkday_test_policy" {
+    name = "checkday_test_policy"
+
+    policy = jsonencode({
+        Statement = [{
+            Action = [
+                "s3:ListAllMyBuckets",
+                "s3:GetBucketLocation"
+            ]
+            Effect = "Allow"
+            Resource = "arn:aws:s3:::*"
+        }]
+        Version = "2012-10-17"
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "checkday_test_attach" {
+    role = aws_iam_role.checkday_test_oidc.name
+    policy_arn = aws_iam_policy.checkday_test_policy.arn
+}
