242b1f1b25e5783b2f3c114dcc2fd1203614a0c3
sstej88 <sunkara.sai@northeastern.edu>
2024-01-15 20:35:42 -0500

    karpenter implemented

--- kubernetes/commands.sh
+++ kubernetes/commands.sh
@@ -4,21 +4,23 @@ aws configure --profile iac
 aws eks update-kubeconfig --region us-east-1 --name checkday_eks_cluster --profile iac
 kubectl config get-contexts
 kubectl config use-context arn:aws:eks:us-east-1:467465390813:cluster/checkday_eks_cluster
-kubectl get nodes
-kubectl get pods
 kubectl describe namespaces
 kubectl create namespace karpenter
-helm list -A
-kubectl get pods -n karpenter
-
+kubectl describe namespaces
 kubectl create secret docker-registry checkday-ecr-secret --docker-server=467465390813.dkr.ecr.us-east-1.amazonaws.com --docker-username=AWS --docker-password=$(aws ecr get-login-password) --docker-email=sunkara.sai+checkday_dev@northeastern.edu
 kubectl create secret docker-registry checkday-docker-hub-secret --docker-username=saitejsunkara --docker-password=Encrypted@1729 --docker-email=sunkara.sai@northeastern.edu
 
 
+kubectl get nodes
+kubectl get pods
+kubectl apply -f kubernetes/karpenter_provisioner.yaml
+helm list -A
 kubectl get pods -n karpenter
+
+
 kubectl describe deployment karpenter -n karpenter
 
-kubectl apply -f kubernetes/karpenter_provisioner.yaml
+
 
 # 2 Shells
 
--- security_groups.tf
+++ security_groups.tf
@@ -47,6 +47,14 @@ resource "aws_security_group" "checkday_db_security_group" {
     security_groups  = [aws_security_group.checkday_app_security_group.id]
   }
 
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   tags = {
     Name = "checkday_db_security_group"
   }
