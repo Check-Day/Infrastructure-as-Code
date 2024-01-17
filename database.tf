9f2f5b473e180bca72c2f944d86c49c4bf935266
sstej88 <sunkara.sai@northeastern.edu>
2024-01-17 05:07:33 -0500

    database subnet group and security groups updated

--- database.tf
+++ database.tf
@@ -40,43 +40,42 @@ resource "aws_iam_role_policy_attachment" "rds_monitoring_policy_attachment" {
   policy_arn = aws_iam_policy.rds_monitoring_policy.arn
 }
 
-resource "aws_db_subnet_group" "checkday_db_subnet_group" {
-  name       = "checkday_db_subnet_group"
-  subnet_ids = [for subnet in aws_subnet.checkday_private_subnet : subnet.id]
-
-  tags = {
-    Name = "checkday_db_subnet_group"
+resource "aws_rds_cluster" "checkday_database" {
+  cluster_identifier      = "checkday-aurora-db"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.11.4"
+  availability_zones      = var.availability_zones
+  database_name           = var.database_name
+  master_username         = var.database_username
+  master_password         = var.database_password
+  backup_retention_period = 1
+  preferred_backup_window = "07:00-09:00"
+  final_snapshot_identifier = "checkday-aurora-db-final-snapshot-${formatdate("YYYYMMDDHHmmss", timestamp())}"
+  # skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.checkday_db_security_group.id]
+  db_subnet_group_name = aws_db_subnet_group.checkday_db_subnet_group.name
+  engine_mode = "serverless"
+  scaling_configuration {
+    min_capacity = 1
+    max_capacity = 8
   }
 }
 
-resource "aws_rds_cluster" "checkday_database" {
-  cluster_identifier       = "checkday-aurora-db"
-  engine                   = "aurora-mysql"
-  engine_version           = "5.7.mysql_aurora.2.11.2"
-  availability_zones       = var.availability_zones
-  database_name            = var.database_name
-  master_username          = var.database_username
-  master_password          = var.database_password
-  backup_retention_period  = 5
-  preferred_backup_window  = "07:00-09:00"
-  final_snapshot_identifier = "checkday-db-final-snapshot-${formatdate("YYYYMMDDHHmmss", timestamp())}"
-  db_subnet_group_name     = aws_db_subnet_group.checkday_db_subnet_group.name
-}
+# resource "aws_rds_cluster_instance" "checkday_rds_cluster_instance" {
+#   count              = 2
+#   identifier         = "checkday-aurora-instance-${count.index}"
+#   cluster_identifier = aws_rds_cluster.checkday_database.id
+#   instance_class     = "db.t2.xlarge"
+#   publicly_accessible = true
+#   monitoring_role_arn = aws_iam_role.rds_monitoring_role.arn
+#   db_subnet_group_name = aws_db_subnet_group.checkday_db_subnet_group.name
+#   apply_immediately = true
+#   availability_zone = var.checkday_rds_instance_availability_zone
+#   engine = "aurora-mysql"
+# }
 
-resource "aws_rds_cluster_instance" "checkday_database_cluster_instance" {
-  apply_immediately  = true
-  cluster_identifier = aws_rds_cluster.checkday_database.id
-  identifier         = "checkday-aurora-db-instance"
-  instance_class     = "db.t2.small"
-  engine             = aws_rds_cluster.checkday_database.engine
-  engine_version     = aws_rds_cluster.checkday_database.engine_version
-  db_subnet_group_name = aws_db_subnet_group.checkday_db_subnet_group.name
-  monitoring_role_arn = aws_iam_role.rds_monitoring_role.arn
-  monitoring_interval = 60
-}
-
-resource "aws_rds_cluster_endpoint" "checkday_database_cluster_instance_endpoint" {
-  cluster_identifier          = aws_rds_cluster.checkday_database.id
-  cluster_endpoint_identifier = "static"
-  custom_endpoint_type        = "ANY"
-}
+# variable "checkday_rds_instance_availability_zone" {
+#   type = string
+#   description = "Checkday database instance availability zone"
+#   default = "us-east-1a"
+# }
--- security_groups.tf
+++ security_groups.tf
@@ -1,39 +1,3 @@
-resource "aws_security_group" "checkday_load_balancer_security_group" {
-  name        = "checkday_load_balancer_security_group"
-  description = "Load Balancer Trafic Management"
-  vpc_id      = aws_vpc.checkday_vpc.id
-
-  ingress {
-    description      = "Site Traffic"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description      = "Secured Site Traffic"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "checkday_load_balancer_security_group"
-  }
-}
-
 resource "aws_security_group" "checkday_db_security_group" {
   name        = "checkday_db_security_group"
   description = "Database Traffic Management"
@@ -44,7 +8,7 @@ resource "aws_security_group" "checkday_db_security_group" {
     from_port        = 3306
     to_port          = 3306
     protocol         = "tcp"
-    security_groups  = [aws_security_group.checkday_app_security_group.id]
+    security_groups  = [aws_eks_cluster.checkday_eks_cluster.vpc_config[0].cluster_security_group_id]
   }
 
   egress {
@@ -60,38 +24,11 @@ resource "aws_security_group" "checkday_db_security_group" {
   }
 }
 
-resource "aws_security_group" "checkday_app_security_group" {
-  name        = "checkday_app_security_group"
-  description = "App Traffic Management"
-  vpc_id      = aws_vpc.checkday_vpc.id
-
-  dynamic "ingress" {
-    for_each = var.appPorts
-
-    content {
-      description      = "App Traffic"
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      security_groups  = [aws_security_group.checkday_load_balancer_security_group.id]
-    }
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+resource "aws_db_subnet_group" "checkday_db_subnet_group" {
+  name       = "checkday_db_subnet_group"
+  subnet_ids = concat([for subnet in aws_subnet.checkday_private_subnet : subnet.id])
 
   tags = {
-    Name = "checkday_app_security_group"
+    Name = "checkday_db_subnet_group"
   }
-}
-
-variable "appPorts" {
-  type        = list(number)
-  description = "Describes app port"
-  default     = [22, 5969]
 }
